8f1fe7ed2884d4fb0989b6855ac0fffd5a7573e0
AmerAmora <Amer.m.amora@gmail.com>
2022-11-03 16:11:18 +0300

    js tasks

---
+++ Js/Task1.js
@@ -0,0 +1,26 @@
+var x=90;
+if(0<=x && x<50)
+{
+    document.write("Failed");
+}
+ else if(50<=x && x<60)
+{
+    document.write("D");
+}
+else if(60<=x && x<70)
+{
+    document.write("C");
+}
+else if(70<=x && x<80)
+{
+    document.write("B");
+}
+else if(80<=x && x<90)
+{
+    document.write("A");
+}
+else if(90<=x && x<=100)
+{
+    document.write("A+");
+}
+else {document.write("Invalid mark");}
---
+++ Js/Task2.js
@@ -0,0 +1,4 @@
+const pi=3.14;
+let r = 10;
+var area = pi *r*r;
+document.write(area);
